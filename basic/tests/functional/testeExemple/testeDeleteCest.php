4f3f469b171720bfbf0a10c529b2ada10c37341a
Allan Kaio <75497513+AllanKaio21@users.noreply.github.com>
2023-03-01 15:02:33 -0300

    Update testeDeleteCest.php

--- basic/tests/functional/testeExemple/testeDeleteCest.php
+++ basic/tests/functional/testeExemple/testeDeleteCest.php
@@ -6,7 +6,6 @@ class TesteDelete
     {
         $I->expectTo('Verificar se a deleção de informações de uma pessoa está correta.');
         $model = $I->grabRecord('app\models\Teste', array('nome' => 'Jorge'));
-        $I->sendAjaxPostRequest(['/Teste/delete', 'id' => $model->id]);
         $I->amOnRoute('teste/delete', ['id' => $model->id]);
         $I->dontSeeRecord('app\models\Teste', [
             'id' => $id,
