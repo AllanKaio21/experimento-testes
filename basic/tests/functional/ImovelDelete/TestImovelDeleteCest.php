99223bae9579661ffd969368c6dee314e85dd099
YasminTXJ <99887005+YasminTXJ@users.noreply.github.com>
2023-03-25 00:29:25 +0000

    Roteiro 1

---
+++ basic/tests/functional/ImovelDelete/TestImovelDeleteCest.php
@@ -0,0 +1,26 @@
+<?php
+class TestImovelDeleteCest
+{
+    public function _before(FunctionalTester $I){
+        // TODO: Enter a login method if needed!
+    }
+
+    //Test Delete
+    public function ImovelDelete(FunctionalTester $I)
+    {
+        $I->wantTo('Verify exception for Delete');
+        $model = $I->grabRecord('app\models\Imovel', array(
+            'nome' => 'Form Tester 002',
+            'cep' => '75124321',
+            'rua' => 'Form Tester 001',
+            'complemento' => 'Form Tester 002',
+            'valor' => '150000',
+            // TODO: Fill the string with the last value of the data in the update
+        ));
+        $id = $model->id;
+        $I->amOnRoute('/imovel/delete', ['id' => $id]);
+        $I->dontSeeRecord('app\models\Imovel', [
+            'id'=> $id,
+        ]);
+    }
+}
